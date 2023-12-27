53c1659549bf3270405bdccb8779d10a7ecf66bc
Guus der Kinderen <guus.der.kinderen@gmail.com>
2023-12-27 14:12:03 +0100

    Code maintenance
    Added a logger, fixed naming convention for class name, introduces SystemProperty to replace JiveGlobal, use type-safe JID instead of strings, and applied various IDE suggested improvements to the code.

--- src/java/com/tempstop/EmailOnAwayPlugin.java
+++ src/java/com/tempstop/EmailOnAwayPlugin.java
@@ -15,22 +15,38 @@ import org.jivesoftware.openfire.PresenceManager;
 import org.jivesoftware.openfire.vcard.VCardManager;
 import org.jivesoftware.util.JiveGlobals;
 import org.jivesoftware.util.EmailService;
+import org.jivesoftware.util.SystemProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xmpp.packet.JID;
 import org.xmpp.packet.Message;
 import org.xmpp.packet.Packet;
 
 //import java.io.File;
 import java.io.*;
 
-public class emailOnAway implements Plugin, PacketInterceptor {
+public class EmailOnAwayPlugin implements Plugin, PacketInterceptor
+{
+    /**
+     * Controls if the e-mail address of the intended recipient is sent in the confirmation message that is sent to the
+     * originator.
+     */
+    public final SystemProperty<Boolean> SHOW_EMAIL = SystemProperty.Builder.ofType(Boolean.class)
+        .setKey("plugin.emailonaway.showemail")
+        .setPlugin("Email on Away")
+        .setDefaultValue(true)
+        .setDynamic(true)
+        .build();
 
-    private InterceptorManager interceptorManager;
-    private UserManager userManager;
-    private PresenceManager presenceManager;
-    private EmailService emailService;
-    private MessageRouter messageRouter;
-    private VCardManager vcardManager;
+    private static final Logger Log = LoggerFactory.getLogger(EmailOnAwayPlugin.class);
+    private final InterceptorManager interceptorManager;
+    private final UserManager userManager;
+    private final PresenceManager presenceManager;
+    private final EmailService emailService;
+    private final MessageRouter messageRouter;
+    private final VCardManager vcardManager;
 
-    public emailOnAway() {
+    public EmailOnAwayPlugin() {
         interceptorManager = InterceptorManager.getInstance();
         emailService = EmailService.getInstance();
         messageRouter = XMPPServer.getInstance().getMessageRouter();
@@ -49,44 +65,46 @@ public class emailOnAway implements Plugin, PacketInterceptor {
         interceptorManager.removeInterceptor(this);
     }
 
-    private Message createServerMessage(String to, String from, String emailTo) {
-        Message message = new Message();
+    private Message createServerMessage(JID to, JID from, String emailTo) {
+        final Message message = new Message();
         message.setTo(to);
         message.setFrom(from);
         message.setSubject("I'm away");
-        if (JiveGlobals.getBooleanProperty("plugin.emailonaway.showemail", true)) {
+        if (SHOW_EMAIL.getValue()) {
             message.setBody("I'm currently away. Your message has been forwarded to my email address  (" + emailTo + ").");
-        } else { message.setBody("I'm currently away. Your message has been forwarded to my email address."); }
+        } else {
+            message.setBody("I'm currently away. Your message has been forwarded to my email address.");
+        }
         return message;
     }
 
     public void interceptPacket(Packet packet, Session session, boolean read,
-                                boolean processed) throws PacketRejectedException {
-
-        String emailTo = null;
-        String emailFrom = null;
+                                boolean processed) throws PacketRejectedException
+    {
+        String emailTo;
+        String emailFrom;
 
         if((!processed) &&
             (!read) &&
             (packet instanceof Message) &&
-            (packet.getTo() != null)) {
-
+            (packet.getTo() != null))
+        {
             Message msg = (Message) packet;
 
             if(msg.getType() == Message.Type.chat) {
                 try {
                     User userTo = userManager.getUser(packet.getTo().getNode());
-                    if(presenceManager.getPresence(userTo).toString().toLowerCase().indexOf("away") != -1) {
+                    if(presenceManager.getPresence(userTo).toString().toLowerCase().contains("away")) {
                         // Status isn't away
                         if(msg.getBody() != null) {
                             // Build email/sms
                             // The to email address
                             emailTo = vcardManager.getVCardProperty(userTo.getUsername(), "EMAIL");
-                            if(emailTo == null || emailTo.length() == 0) {
+                            if(emailTo == null || emailTo.isEmpty()) {
                                 emailTo = vcardManager.getVCardProperty(userTo.getUsername(), "EMAIL:USERID");
-                                if(emailTo == null || emailTo.length() == 0) {
+                                if(emailTo == null || emailTo.isEmpty()) {
                                     emailTo = userTo.getEmail();
-                                    if(emailTo == null || emailTo.length() == 0) {
+                                    if(emailTo == null || emailTo.isEmpty()) {
                                         emailTo = packet.getTo().getNode() + "@" + packet.getTo().getDomain();
                                     }
                                 }
@@ -94,17 +112,16 @@ public class emailOnAway implements Plugin, PacketInterceptor {
                             // The From email address
                             User userFrom = userManager.getUser(packet.getFrom().getNode());
                             emailFrom = vcardManager.getVCardProperty(userFrom.getUsername(), "EMAIL");
-                            if(emailFrom == null || emailFrom.length() == 0) {
+                            if(emailFrom == null || emailFrom.isEmpty()) {
                                 emailFrom = vcardManager.getVCardProperty(userFrom.getUsername(), "EMAIL:USERID");
-                                if(emailFrom == null || emailFrom.length() == 0) {
+                                if(emailFrom == null || emailFrom.isEmpty()) {
                                     emailFrom = userFrom.getEmail();
-                                    if(emailFrom == null || emailFrom.length() == 0) {
+                                    if(emailFrom == null || emailFrom.isEmpty()) {
                                         emailFrom = packet.getFrom().getNode() + "@" + packet.getFrom().getDomain();
                                     }
                                 }
                             }
 
-//			    System.err.println(vcardManager.getVCardProperty(userTo.getUsername(), "EMAIL:USERID"));
                             // Send email/sms
                             // if this is an sms... modify the recipient address
                             emailService.sendMessage(userTo.getName(),
@@ -116,11 +133,11 @@ public class emailOnAway implements Plugin, PacketInterceptor {
                                 null);
 
                             // Notify the sender that this went to email/sms
-                            messageRouter.route(createServerMessage(packet.getFrom().getNode() + "@" + packet.getFrom().getDomain(), packet.getTo().getNode() + "@" + packet.getTo().getDomain(), emailTo));
-
+                            messageRouter.route(createServerMessage(packet.getFrom().asBareJID(), packet.getTo().asBareJID(), emailTo));
                         }
                     }
                 } catch (UserNotFoundException e) {
+                    Log.debug("Unable to determine if an email should be sent to a user that is away, as the user '{}' cannot be found.", packet.getTo(), e);
                 }
             }
         }
